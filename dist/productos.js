bb437a4c8aecf78ce269df091b57a294ad74be73
DanLeon1 <danielleones402@gmail.com>
2023-02-24 03:05:09 -0500

    footer daniel

---
+++ dist/productos.js
@@ -0,0 +1,36 @@
+const btnProof = document.getElementById('botonproof');
+const botonpro = document.getElementById('botonpro');
+
+btnProof.addEventListener("click", eventobtnProo);
+
+
+function eventobtnProo(e) {
+    e.preventDefault();
+    Swal.fire({
+        title:'añadido al carrito',
+        imageUrl: '../assets/img/carritooo.png',
+        imageHeight: 150,
+        
+      })
+     
+
+
+}
+
+btnProof.addEventListener("click", eventobotonpro);
+
+
+function eventobotonpro(e) {
+    e.preventDefault();
+    Swal.fire({
+        title:'añadido al carrito',
+        imageUrl: '../assets/img/carritooo.png',
+        imageHeight: 150,
+        
+      })
+     
+
+
+}
+
+
